0f7f623c983f35f9bc4c6e6bb4fd8bdfe5bb76fa
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2017-10-12 18:38:06 +0300

    Add CSS

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
-INSERT INTO Customer VALUES (null, 'David', 'Adams', 'd.adams@gmail.com'),
+INSERT INTO Customer (id, first_name, last_name, email)
+VALUES (null, 'David', 'Adams', 'd.adams@gmail.com'),
 (NULL, 'John', 'Doe', 'j.doe@gmail.com'),
 (NULL , 'Ajay', 'Rao', 'a.rao@gmail.com'),
 (NULL , 'Mary', 'Public', 'm.public@gmail.com'),
